3b972cd538c4842fed41626306efb06d8cfe2651
Your Name <you@example.com>
2022-08-31 23:47:33 +0530

    [Megha] Add: Retrieved Salary Details of aprticular Employee

--- EmployeePayrollService.sql
+++ EmployeePayrollService.sql
@@ -13,9 +13,9 @@ create table employee_payroll
  StartDate date
 );
 
-Select * from employee_payroll
 
 ----UC3------
+
 Insert Into employee_payroll(Name, Salary, StartDate) values('Megha',2200, '2021-04-22')
 Insert Into employee_payroll(Name, Salary, StartDate) values('Rahul',3500, '2022-05-23')
 Insert Into employee_payroll(Name, Salary, StartDate) values('Savita',3466, '2021-06-24')
@@ -23,3 +23,9 @@ Insert Into employee_payroll(Name, Salary, StartDate) values('Prabhu',9650, '202
 
 ----UC4-----
 select * from employee_payroll
+
+-----UC5------
+
+Select Salary from employee_payroll where Name = 'Megha'
+
+Select Salary from employee_payroll where StartDate Between CAST('2022-07-25' as date) and GETDATE()
