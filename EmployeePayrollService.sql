35b443aecfa0ecc04264742ec038cf1b43c66146
Your Name <you@example.com>
2022-08-31 23:40:03 +0530

    [Megha] Add: data Inserted Into The Table

--- EmployeePayrollService.sql
+++ EmployeePayrollService.sql
@@ -14,3 +14,9 @@ create table employee_payroll
 );
 
 Select * from employee_payroll
+
+----UC3------
+Insert Into employee_payroll(Name, Salary, StartDate) values('Megha',2200, '2021-04-22')
+Insert Into employee_payroll(Name, Salary, StartDate) values('Rahul',3500, '2022-05-23')
+Insert Into employee_payroll(Name, Salary, StartDate) values('Savita',3466, '2021-06-24')
+Insert Into employee_payroll(Name, Salary, StartDate) values('Prabhu',9650, '2022-07-25')
